85ac7dbd886dad628cd2e4b6ad6a00d12efa9775
DanielJohn17 <danielyohannes38@gmail.com>
2024-11-20 20:12:48 +0300

    Added custom color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,12 +4,21 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "custom-color-yelow": "#FAEF5C",
+        "custom-color-green": {
+          300: "#98D86F",
+          400: "#41B886",
+        },
+        "custom-color-blue": {
+          300: "#00938D",
+          400: "#106D7D",
+          900: "#2F4858",
+        },
       },
     },
   },
