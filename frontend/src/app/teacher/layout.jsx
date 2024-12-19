8ba4f88bdefb0fd99d89e9ad70a6ae7b232e07ed
snoozypanda <yeabtsegamekonnen04@gmail.com>
2024-12-19 08:54:48 +0300

    Version3

--- frontend/src/app/teacher/layout.jsx
+++ frontend/src/app/teacher/layout.jsx
@@ -2,38 +2,26 @@
 import { useState } from "react";
 import TeacherSideBar from "@containers/TeacherSideBar";
 
-export default function TeachersLayout({ dashboard, attendance, grade }) {
-  const [dashboardActive, setDashboardActive] = useState(true);
-  const [studentAttendanceActive, setStudentAttendanceActive] = useState(false);
-  const [studentGradeActive, setStudentGradeActive] = useState(false);
+export default function TeachersLayout({
+  dashboard,
+  attendance,
+  grade,
+  Schedule,
+}) {
+  const [activeSection, setActiveSection] = useState("dashboard");
 
   const handleActive = (active) => {
-    setDashboardActive(false);
-    setStudentAttendanceActive(false);
-    setStudentGradeActive(false);
-
-    switch (active) {
-      case "dashboard":
-        setDashboardActive(true);
-        break;
-      case "student_attendance":
-        setStudentAttendanceActive(true);
-        break;
-      case "student_grade":
-        setStudentGradeActive(true);
-        break;
-      default:
-        break;
-    }
+    setActiveSection(active);
   };
 
   return (
     <div className="h-screen w-screen flex">
       <TeacherSideBar handleActive={handleActive} />
       <div className="flex-grow">
-        {dashboardActive && dashboard}
-        {studentAttendanceActive && attendance}
-        {studentGradeActive && grade}
+        {activeSection === "dashboard" && dashboard}
+        {activeSection === "student_attendance" && attendance}
+        {activeSection === "student_grade" && grade}
+        {activeSection === "schedule" && Schedule}
       </div>
     </div>
   );
